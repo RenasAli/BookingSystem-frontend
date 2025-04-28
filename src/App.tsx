53c2ee052c8e40ec470e0b2bf74921e9d41fdb08
RenasAli <rena0506@stud.kea.dk>
2025-04-28 14:38:28 +0200

    fis deploy issue

--- src/App.tsx
+++ src/App.tsx
@@ -10,19 +10,21 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
     <Flex>
-      <Show above="lg" >
-        <DesktopSidebar/>
-      </Show>
+      
       <Container>
         <Routes>
+            <Show above="lg" >
+              <DesktopSidebar/>
+            </Show>
+            <Hide above="lg">
+              <MobileSidebar/>
+            </Hide>
             <Route path="/login" element={<Login />} />
             <Route path="/dashboard/bookings" element={<Bookings />} />
             <Route path="/dashboard/staff" element={<Staff />} />
         </Routes>
       </Container>
-      <Hide /*above="lg"*/>
-        <MobileSidebar/>
-      </Hide>
+      
       
     </Flex>
     </QueryClientProvider>
