6d7ae2201863d4904a56ede2bd06e9f6f54187c6
RenasAli <rena0506@stud.kea.dk>
2025-05-10 11:23:14 +0200

    theme to sidebar

--- src/theme.ts
+++ src/theme.ts
@@ -2,9 +2,42 @@ import { extendTheme, ThemeConfig } from "@chakra-ui/react";
 
 const config: ThemeConfig = {
   initialColorMode: "light",
-  useSystemColorMode: false
+  useSystemColorMode: false,
+  
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config,
+  components: {
+    Button: {
+      variants: {
+        nav: {
+          h: "50px",
+          w: "full",
+          fontSize: "20px",
+          justifyContent: "flex-start",
+          bg: "#579AFF",
+          color: "white",
+          borderRadius: "0px",
+          _hover: {
+            bg: "#3674d1",
+          }
+        },
+        selected_nav: {
+          h: "50px",
+          w: "full",
+          fontSize: "20px",
+          justifyContent: "flex-start",
+          bg: "#3674d1",
+          color: "white",
+          borderRadius: "0px",
+          _hover: {
+            bg: "#3674d1",
+          }
+        }
+      },
+    },
+  }
+});
 
 export default theme;
