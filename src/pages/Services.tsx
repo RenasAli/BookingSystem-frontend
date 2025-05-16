6bd124f11e0681a872406ce987ad1e61c7cd2846
chris <chri402s@stud.kea.dk>
2025-05-16 11:39:00 +0200

    made delete option for staff and service like in offdays

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,28 +1,56 @@
-import { Button, SimpleGrid, useDisclosure} from "@chakra-ui/react"
+import { Button, SimpleGrid, useDisclosure, useToast } from "@chakra-ui/react"
 import useService from "../hooks/useService";
 import Service from "../types/Service";
 import { ServiceCard, ServiceModal } from "../components";
 import { useState } from "react";
 import { useCreateMutation } from "../hooks/useCreateMutation";
+import { useDeleteMutation } from "../hooks/useDeleteMutation";
 import { useQueryClient } from "@tanstack/react-query";
+import ConfirmDialogBox from "../components/ConfirmDialogBox";
 import { RoleGuard } from "../auth/RoleGuard";
 
 const Services = () => {
   const queryClient = useQueryClient();
+  const toast = useToast();
 
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen: isOpenDeleteModal, onOpen: onOpenDeleteModal, onClose: onCloseDeleteModal } = useDisclosure();
   const [service, setservice] = useState<Service>();
+  const [serviceDeleteId, setServiceDeleteId] = useState<number>();
+
   const serviceQuery = useService();
-  const updateMutation = useCreateMutation<Service>({ endpoint: `service/${service?.id}`, method: "PUT", onSuccess: async() => {
-      await queryClient.invalidateQueries({queryKey: ['services', service?.id]});
+
+  const updateMutation = useCreateMutation<Service>({
+    endpoint: `service/${service?.id}`, method: "PUT", onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ['services', service?.id] });
       await queryClient.refetchQueries({ queryKey: ["services",] });
       setservice(undefined)
-  } });
-  const createMutation = useCreateMutation<Service>({ endpoint: `service`, method: "POST", onSuccess: async() => {
-    await queryClient.invalidateQueries({
+    }
+  });
+
+  const createMutation = useCreateMutation<Service>({
+    endpoint: `service`, method: "POST", onSuccess: async () => {
+      await queryClient.invalidateQueries({
         queryKey: ['services']
       })
-  } });
+    }
+  });
+
+  const deleteMutation = useDeleteMutation({
+    endpoint: `service/${serviceDeleteId}`,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ["services"] });
+      await queryClient.refetchQueries({ queryKey: ["services"] });
+      onCloseDeleteModal();
+      toast({
+        title: "Service slettet",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+    },
+  });
+
   if (serviceQuery.isLoading) {
     return <div>Loading...</div>;
   }
@@ -35,10 +63,16 @@ const Services = () => {
     setservice(undefined);
     onOpen();
   }
+
   const handleOnOpenEditModal = (service: Service) => {
     onOpen();
     setservice(service);
   }
+
+  const handleDeleteService = (id: number) => {
+    setServiceDeleteId(id);
+    onOpenDeleteModal();
+  };
 
   const updateServiceField = (field: keyof Service, value: string) => {
     setservice((prev) => ({ ...prev, [field]: value } as Service));
@@ -49,36 +83,50 @@ const Services = () => {
 
     if (service?.id) {
       updateMutation.mutate(service);
-    } else if(service) {
+    } else if (service) {
       createMutation.mutate(service);
     }
     onClose();
   };
+
   return (
     <>
-    <RoleGuard allowedRoles={["company_admin"]}>
-      <Button variant="success" mr={3} mb={5} onClick={handleOnOpenCreateModal}>
-        Opret Service
-      </Button>
-    </RoleGuard>
-    <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
-        {serviceQuery?.data?.map((service: Service, index: number) => {
-          return <ServiceCard
-          key={index}
-          id={service.id}
-          name={service.name}
-          description={service.description}
-          price={service.price}
-          durationMinutes={service.durationMinutes}
-          onOpen={handleOnOpenEditModal}
-        />;
-        })}
-    </SimpleGrid>
-    <RoleGuard allowedRoles={["company_admin"]}>
-      <ServiceModal isOpen={isOpen} onClose={onClose} service={service} onChange= {updateServiceField} onSubmit={handleSubmit} isEditing={!!service?.id}/>
-    </RoleGuard>
+      <RoleGuard allowedRoles={["company_admin"]}>
+        <Button variant="success" mr={3} mb={5} onClick={handleOnOpenCreateModal}>
+          Opret Service
+        </Button>
+      </RoleGuard>
+      <SimpleGrid spacing={4} templateColumns="repeat(auto-fill, minmax(200px, 1fr))">
+        {serviceQuery?.data?.map((service: Service) => (
+          <ServiceCard
+            key={service.id}
+            service={service}
+            onOpen={handleOnOpenEditModal}
+            getDeleteId={handleDeleteService}
+          />
+        ))}
+      </SimpleGrid>
+      <RoleGuard allowedRoles={["company_admin"]}>
+        <ServiceModal
+          isOpen={isOpen}
+          onClose={onClose}
+          service={service}
+          onChange={updateServiceField}
+          onSubmit={handleSubmit}
+          isEditing={!!service?.id}
+        />
+      </RoleGuard>
+      <ConfirmDialogBox
+        header="Warning"
+        description="Er du sikker på, at du vil fjerne den service?"
+        confirmButtonText="Slet"
+        confirmIsLoading={deleteMutation.isPending}
+        isOpen={isOpenDeleteModal}
+        handleConfirmClick={deleteMutation.mutate}
+        onClose={onCloseDeleteModal}
+      />
     </>
-  )
-}
+  );
+};
 
 export default Services
--- src/pages/Staff.tsx
+++ src/pages/Staff.tsx
@@ -1,32 +1,61 @@
-import { Button, SimpleGrid, useDisclosure} from "@chakra-ui/react"
+import { Button, SimpleGrid, useDisclosure, useToast } from "@chakra-ui/react"
 import useStaff from "../hooks/useStaff";
 import Staffs from "../types/Staff";
 import { StaffCard } from "../components";
 import { useState } from "react";
 import { useCreateMutation } from "../hooks/useCreateMutation";
+import { useDeleteMutation } from "../hooks/useDeleteMutation";
 import { useQueryClient } from "@tanstack/react-query";
 import StaffModal from "../components/StaffModal";
+import ConfirmDialogBox from "../components/ConfirmDialogBox";
 import { RoleGuard } from "../auth/RoleGuard";
 
 const Staff = () => {
   const queryClient = useQueryClient();
+  const toast = useToast();
+
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen: isOpenDeleteModal, onOpen: onOpenDeleteModal, onClose: onCloseDeleteModal } = useDisclosure();
   const [staff, setStaff] = useState<Staffs>();
+  const [staffDeleteId, setStaffDeleteId] = useState<number>();
+  
   const staffQuery = useStaff();
-  const updateMutation = useCreateMutation<Staffs>({ endpoint: `staff/${staff?.id}`, method: "PUT", onSuccess: async() => {
-      await queryClient.invalidateQueries({queryKey: ['staff', staff?.id]});
+
+  const updateMutation = useCreateMutation<Staffs>({
+    endpoint: `staff/${staff?.id}`, method: "PUT", onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ['staff', staff?.id] });
       await queryClient.refetchQueries({ queryKey: ["staff",] });
       setStaff(undefined)
-  } });
-  const createMutation = useCreateMutation<Staffs>({ endpoint: `staff`, method: "POST", onSuccess: async() => {
-    await queryClient.invalidateQueries({
+    }
+  });
+
+  const createMutation = useCreateMutation<Staffs>({
+    endpoint: `staff`, method: "POST", onSuccess: async () => {
+      await queryClient.invalidateQueries({
         queryKey: ['staff']
       })
-  } });
+    }
+  });
+
+  const deleteMutation = useDeleteMutation({
+    endpoint: `staff/${staffDeleteId}`,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ["staff"] });
+      await queryClient.refetchQueries({ queryKey: ["staff"] });
+      onCloseDeleteModal();
+      toast({
+        title: "Medarbejder slettet",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+    },
+  });
+
   if (staffQuery.isLoading) {
     return <div>Loading...</div>;
   }
-    
+
   if (staffQuery.isError) {
     return <div>Error loading...</div>;
   }
@@ -37,6 +66,7 @@ const Staff = () => {
     setStaff(undefined);
     onOpen();
   }
+
   const handleOnOpenEditModal = (staff: Staffs) => {
     onOpen();
     setStaff(staff);
@@ -45,37 +75,42 @@ const Staff = () => {
   const updateStaffField = (field: keyof Staffs, value: unknown) => {
     //setStaff((prev) => ({ ...prev, [field]: value } as Staffs));
     setStaff((prev) => {
-          if (!prev) return prev;
-      
-          const updatedStaff = { ...prev };
-      
-          switch (field) {
-            case "name":
-              updatedStaff.name = value as string;
-              break;
-            case "email":
-              updatedStaff.email = value as string;
-              break;
-            case "phone":
-              updatedStaff.phone = value as string;
-              break;
-            case "staffWorkdays":
-              updatedStaff.staffWorkdays = value as Staffs["staffWorkdays"];
-              break;
-            default:
-              console.warn(`Unhandled field in updateCompanyField: ${field}`);
-          }
-      
-          return updatedStaff;
-        });
+      if (!prev) return prev;
+
+      const updatedStaff = { ...prev };
+
+      switch (field) {
+        case "name":
+          updatedStaff.name = value as string;
+          break;
+        case "email":
+          updatedStaff.email = value as string;
+          break;
+        case "phone":
+          updatedStaff.phone = value as string;
+          break;
+        case "staffWorkdays":
+          updatedStaff.staffWorkdays = value as Staffs["staffWorkdays"];
+          break;
+        default:
+          console.warn(`Unhandled field in updateCompanyField: ${field}`);
+      }
+
+      return updatedStaff;
+    });
+  };
+
+  const handleDeleteStaff = (id: number) => {
+    setStaffDeleteId(id);
+    onOpenDeleteModal();
   };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-  
+
     if (staff?.id) {
       updateMutation.mutate(staff);
-    } else if(staff) {
+    } else if (staff) {
       createMutation.mutate(staff);
     }
     onClose();
@@ -83,26 +118,44 @@ const Staff = () => {
 
   return (
     <>
-    <RoleGuard allowedRoles={["company_admin"]}>
-      <Button variant="success" mr={3} mb={5} onClick={handleOnOpenCreateModal}>
-        Opret Medarbejder
-      </Button>
-    </RoleGuard>
-    <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
-        {staffData.map((staff: Staffs, index: number) => {
-          return <StaffCard key= {index} id={staff.id} name={staff.name} email={staff.email} phone={staff.phone} staffWorkdays={staff.staffWorkdays} onOpen={handleOnOpenEditModal}/>;
-        })}
-    </SimpleGrid>
-    <RoleGuard allowedRoles={["company_admin"]}>
-      <StaffModal
-        isOpen={isOpen}
-        onClose={onClose}
-        staff={staff}
-        onChange={updateStaffField}
-        onSubmit={handleSubmit}
-        isEditing={!!staff?.id}
-      />
-    </RoleGuard>
+      <RoleGuard allowedRoles={["company_admin"]}>
+        <Button variant="success" mr={3} mb={5} onClick={handleOnOpenCreateModal}>
+          Opret Medarbejder
+        </Button>
+      </RoleGuard>
+      <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
+        {staffData.map((staff: Staffs, index: number) => (
+          <StaffCard
+            key={index}
+            id={staff.id}
+            name={staff.name}
+            email={staff.email}
+            phone={staff.phone}
+            staffWorkdays={staff.staffWorkdays}
+            onOpen={handleOnOpenEditModal}
+            getDeleteId={handleDeleteStaff}
+          />
+        ))}
+      </SimpleGrid>
+      <RoleGuard allowedRoles={["company_admin"]}>
+        <StaffModal
+          isOpen={isOpen}
+          onClose={onClose}
+          staff={staff}
+          onChange={updateStaffField}
+          onSubmit={handleSubmit}
+          isEditing={!!staff?.id}
+        />
+      </RoleGuard>
+      <ConfirmDialogBox
+              header="Warning"
+              description="Er du sikker på, at du vil fjerne den medarbejder?"
+              confirmButtonText="Delete"
+              confirmIsLoading={deleteMutation.isPending}
+              isOpen={isOpenDeleteModal}
+              handleConfirmClick={deleteMutation.mutate}
+              onClose={onCloseDeleteModal}
+            />
     </>
   )
 }
